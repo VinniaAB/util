a70868a7a4459baeef01393a1712c4d6ccad696b
Johan Björk <johanimon@gmail.com>
2016-04-14 00:56:59 +0200

    Add db schemas for testing

---
+++ tests/_data/mysql.sql
@@ -0,0 +1,5 @@
+create table `car` (
+  `car_id` int primary key not null auto_increment,
+  `make` text not null,
+  `model` text not null
+);
---
+++ tests/_data/sqlite.sql
@@ -0,0 +1,5 @@
+create table "car" (
+  "car_id" integer primary key not null,
+  "make" text not null,
+  "model" text not null
+);
